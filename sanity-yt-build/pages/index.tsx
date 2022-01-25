487076a72d41bb0994d5ab05148dcde86e6eb98f
Sarvesh <sarveshjha23@gmail.com>
2022-01-25 11:59:34 +0530

    sanity

---
+++ sanity-yt-build/pages/index.tsx
@@ -0,0 +1,93 @@
+import Head from "next/head";
+import Header from "../Components/Header";
+import { sanityClient, urlFor } from "../sanity";
+import { Post } from "../typings";
+import Link from "next/link";
+
+interface Props {
+  posts: Post[];
+}
+
+export default function Home({ posts }: Props) {
+  return (
+    <div className="max-w-7xl mx-auto">
+      <Head>
+        <title>Medium Blog</title>
+      </Head>
+      <Header />
+
+      <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+        <div className="px-10 space-y-5">
+          <h1 className="text-6xl max-w-xl font-serif">
+            <span className="underline decoration-black decoration-2">
+              Medium
+            </span>
+            Is a place to write read and connect
+          </h1>
+          <h2>
+            It's easy and free to post your thinking on any topic and connect
+            with millions of readers.
+          </h2>
+        </div>
+        <div>
+          <img
+            className="hidden md:inline-flex h-32 lg:h-full"
+            src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png"
+            alt="med-logo"
+          />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-col-3 gap-3 md:gap-6 p-2 md:p-6">
+        {posts.map((post) => {
+          console.log(post.slug.current);
+          return (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="border-2 rounded-lg group cursor-pointer overflow-hidden">
+                <img
+                  className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out "
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between p-5 bg-white">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p className="text-xs">
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export const getServerSideProps = async () => {
+  const query = `*[_type=='post']{
+    _id,
+    title
+ ,_createdAt
+ ,
+    author->{name,image}
+  ,
+  description,
+  mainImage,
+  slug
+    }`;
+  const posts = await sanityClient.fetch(query);
+  console.log("Hello From SSR");
+
+  console.log(posts);
+
+  return {
+    props: { posts },
+  };
+};
---
+++ sanity-yt-build/pages/post/[slug].tsx
@@ -0,0 +1,196 @@
+import { GetStaticProps } from "next";
+import Header from "../../Components/Header";
+import { sanityClient, urlFor } from "../../sanity";
+import { Post } from "../../typings";
+import PortableText from "react-portable-text";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface Props {
+  post: Post;
+}
+interface IformInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
+
+function Post({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IformInput>();
+
+  const onSubmit: SubmitHandler<IformInput> = async (data) => {
+    fetch("/api/createComment", {
+      method: "POST",
+      body: JSON.stringify(data),
+    })
+      .then(() => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <main>
+      <Header />
+      <img
+        className="w-full h-48 object-cover"
+        src={urlFor(post.mainImage).url()!}
+      />
+      <article className="max-w-3xl mx-auto p-5">
+        <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+        <h2 className="text-xl font-light text-grey-500 mb-2">
+          {post.description}
+        </h2>
+
+        <div className="flex items-center space-x-2">
+          <img
+            className="h-10 rounded-full"
+            src={urlFor(post.author.image).url()!}
+          />
+          <p className="font-extralight text-sm">
+            Blog post By
+            <span className="text-green-500"> {post.author.name}</span> -
+            Punlished at {new Date(post._createdAt).toLocaleString()}
+          </p>
+        </div>
+        <div>
+          <PortableText
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+            content={post.body}
+            serializers={{
+              h1: (props: any) => {
+                <h1 className="text-2xl font-bold my-5" {...props} />;
+              },
+              h2: (props: any) => {
+                <h1 className="text-xl font-bold my-5" />;
+              },
+              li: ({ children }: any) => {
+                <li className="ml-4 list-disc">{children}</li>;
+              },
+              link: ({ href, children }: any) => (
+                <a href={href} className="text-blue-500 hover:underline">
+                  {children}
+                </a>
+              ),
+            }}
+          />
+        </div>
+      </article>
+      <hr className="max-w-lg my-5 mx-auto border-3 border-yellow-500" />
+
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col  p-5 max-w-2xl mx-auto mb-10"
+      >
+        <h3 className="text-sm text-yellow-500">Enjoyed The article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment</h4>
+        <hr className="py-3 mt-2" />
+
+        <input {...register("_id")} type="hidden" name="_id" value={post._id} />
+
+        <label className="block mb-5 ">
+          <span className="text-grey-100"> Name</span>
+          <input
+            {...register("name", { required: true })}
+            className="shadow border rounder py-2 px-3 form-input mt-1 block w-full ring-yellow-200 focus:ring"
+            placeholder="Sarvesh Jha"
+            type="text"
+          />
+        </label>
+        <label className="block mb-5 ">
+          <span className="text-grey-700"> Email</span>
+          <input
+            {...register("email", { required: true })}
+            className="shadow border rounder py-2 px-3 form-input mt-1 block w-full ring-yellow-200 focus:ring"
+            placeholder="Sarvesh Jha"
+            type="text"
+          />
+        </label>
+
+        <label className="block mb-5 ">
+          <span className="text-grey-700"> Comment</span>
+          <textarea
+            {...register("comment", { required: true })}
+            className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-200 focus:ring"
+            placeholder="Sarvesh Jha"
+            rows={8}
+          />
+        </label>
+        <div className="flex  flex-col  p-5">
+          {errors.name && (
+            <span className="text-red-500">-The name Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">-The Email Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">-The Comment Field is required</span>
+          )}
+        </div>
+        <input
+          type="submit"
+          className="shadow bg-yellow-500 hover:bg-yellow:400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+        />
+      </form>
+    </main>
+  );
+}
+export default Post;
+
+export const getStaticPaths = async () => {
+  const query = `*[_type=='post']{
+    _id,
+      slug{current}
+    }`;
+
+  const posts = await sanityClient.fetch(query);
+  const paths = posts.map((post: Post) => ({
+    params: {
+      slug: post.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const query = `*[_type=='post' && slug.current== $slug  ][0]{
+        _id,
+         _createdAt,
+         title,
+         author->{
+        name,
+        image
+    },
+    
+    'comments':*[_type=="comment" && post._ref==^._id && approved==true],
+      description,
+       mainImage,
+       slug,
+       body
+    
+      }`;
+
+  const post = await sanityClient.fetch(query, {
+    slug: params?.slug,
+  });
+
+  if (Object.keys(post).length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: { post },
+    revalidate: 60,
+  };
+};
