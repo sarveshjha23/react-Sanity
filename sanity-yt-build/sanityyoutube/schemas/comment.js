58abd5d415bbb0826eb780bcea0cf2e32071cb99
Sarvesh <sarveshjha23@gmail.com>
2022-01-25 18:41:25 +0530

    finished

---
+++ sanity-yt-build/sanityyoutube/schemas/comment.js
@@ -0,0 +1,18 @@
+export default {
+  name: "comment",
+  type: "document",
+  title: "Comment",
+
+  fields: [
+    { name: "name", type: "string" },
+    {
+      title: "Approved",
+      name: "approved",
+      type: "boolean",
+      description: "Comment wont show on the site without approval",
+    },
+    { name: "email", type: "string" },
+    { name: "comment", type: "text" },
+    { name: "post", type: "reference", to: [{ type: "post" }] },
+  ],
+};
