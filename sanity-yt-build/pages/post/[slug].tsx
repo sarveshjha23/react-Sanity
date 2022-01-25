14b0d2a7f2946b4e5e445d1eac2376d4c70d8ab3
Sarvesh <sarveshjha23@gmail.com>
2022-01-25 19:01:42 +0530

    done

--- sanity-yt-build/pages/post/[slug].tsx
+++ sanity-yt-build/pages/post/[slug].tsx
@@ -67,22 +67,6 @@ function Post({ post }: Props) {
             dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
             projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
             content={post.body}
-            serializers={{
-              h1: (props: any) => {
-                <h1 className="text-2xl font-bold my-5" {...props} />;
-              },
-              h2: (props: any) => {
-                <h1 className="text-xl font-bold my-5" />;
-              },
-              li: ({ children }: any) => {
-                <li className="ml-4 list-disc">{children}</li>;
-              },
-              link: ({ href, children }: any) => (
-                <a href={href} className="text-blue-500 hover:underline">
-                  {children}
-                </a>
-              ),
-            }}
           />
         </div>
       </article>
